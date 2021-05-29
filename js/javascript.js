9a5acd19f42e64cb112828827a040c741216513d
iwanmitowski <iwan.mitowski@gmail.com>
2021-05-29 09:11:53 +0300

    Mobile nav - done

--- js/javascript.js
+++ js/javascript.js
@@ -60,3 +60,19 @@ function onload() {
     previousValue = value;
   });
 }
+
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
