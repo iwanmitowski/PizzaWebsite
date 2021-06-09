f0fcd9cdc9aae9934662df4763cd504525acae5f
Krasimir Nedelchev <kaykayehnn@gmail.com>
2021-06-09 19:21:30 +0300

    Formatting

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@
   // Also used in cart.js
   // TODO: remove this duplication
   function formatPrice(price) {
-    return `${price.toFixed(2)}лв`
+    return `${price.toFixed(2)}лв`;
   }
 
   function attachOrderHandlers() {
@@ -14,30 +14,34 @@
           ".mobile-pizza-header"
         ).textContent;
 
-        var pizzaImageUrl = rootElement.querySelector('.pizza-image').getAttribute('src')
-        var pizzaPrice = +rootElement.querySelector('.price-accent').textContent.slice(0, -2);
+        var pizzaImageUrl = rootElement
+          .querySelector(".pizza-image")
+          .getAttribute("src");
+        var pizzaPrice = +rootElement
+          .querySelector(".price-accent")
+          .textContent.slice(0, -2);
         var pizzaSize = rootElement.querySelector(".size-button.selected");
         if (pizzaSize == null) {
           // No size selected
           iziToast.warning({
-            id: 'warning',
-            title: 'Моля изберете размер',
+            id: "warning",
+            title: "Моля изберете размер",
             // message: 'You forgot important data',
-            position: 'bottomRight',
-            transitionIn: 'flipInX',
-            transitionOut: 'flipOutX'
+            position: "bottomRight",
+            transitionIn: "flipInX",
+            transitionOut: "flipOutX",
           });
         } else {
           pizzaSize = pizzaSize.textContent;
-          
+
           // Get current order
           idbKeyval.get("order").then(function (order) {
-            if(order == null) order = []
+            if (order == null) order = [];
             var cartMatch = order.find(function (p) {
-              return p.name === pizzaName && p.size === pizzaSize 
-            })
-  
-            if(cartMatch != null) {
+              return p.name === pizzaName && p.size === pizzaSize;
+            });
+
+            if (cartMatch != null) {
               cartMatch.quantity++;
             } else {
               order.push({
@@ -45,29 +49,32 @@
                 price: pizzaPrice,
                 size: pizzaSize,
                 imageUrl: pizzaImageUrl,
-                quantity: 1
-              })
+                quantity: 1,
+              });
             }
-  
-            idbKeyval.set('order', order).then(function () {
-              iziToast.success({
-                id: 'success',
-                title: `Добавихте пица ${pizzaName} в количката`,
-                position: 'bottomRight',
-                transitionIn: 'flipInX',
-                transitionOut: 'flipOutX'
-                // iconText: 'star',
+
+            idbKeyval
+              .set("order", order)
+              .then(function () {
+                iziToast.success({
+                  id: "success",
+                  title: `Добавихте пица ${pizzaName} в количката`,
+                  position: "bottomRight",
+                  transitionIn: "flipInX",
+                  transitionOut: "flipOutX",
+                  // iconText: 'star',
+                });
+              })
+              .catch(function (err) {
+                iziToast.error({
+                  id: "error",
+                  title: `Настъпи грешка при добавянето в количката`,
+                  position: "bottomRight",
+                  transitionIn: "flipInX",
+                  transitionOut: "flipOutX",
+                  // iconText: 'star',
+                });
               });
-            }).catch(function (err){
-              iziToast.error({
-                id: 'error',
-                title: `Настъпи грешка при добавянето в количката`,
-                position: 'bottomRight',
-                transitionIn: 'flipInX',
-                transitionOut: 'flipOutX'
-                // iconText: 'star',
-              });
-            }) 
           });
         }
       });
@@ -92,7 +99,7 @@
 
         var newPrice = basePrice + sizePriceIncrement[size];
 
-        priceElement.textContent = formatPrice(newPrice)
+        priceElement.textContent = formatPrice(newPrice);
 
         otherButtons.forEach(function (b) {
           b.classList.remove("selected");
